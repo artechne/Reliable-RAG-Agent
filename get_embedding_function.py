a2aeb55b1f341cd9d372f8cdfb0356353233e140
Yeonjoon Kim <artechnej@gmail.com>
2024-07-19 17:34:06 +0900

    add huggingface embeddings

--- get_embedding_function.py
+++ get_embedding_function.py
@@ -1,10 +1,14 @@
 from langchain_openai import OpenAIEmbeddings
-
-#TODO: Change the model to huggingface SOTA model
-#https://huggingface.co/BM-K/KoSimCSE-roberta-multitask
+from langchain_huggingface import HuggingFaceEmbeddings
+#https://github.com/ssisOneTeam/Korean-Embedding-Model-Performance-Benchmark-for-Retriever
 
 OPEN_AI_TEXT_EMBDEDING_MODEL_3_LARGE = "text-embedding-3-large"
+HUGGING_FACE_MODEL_NAME = "jhgan/ko-sroberta-multitask"
 
-def get_embedding_function():
-    embeddings = OpenAIEmbeddings(model=OPEN_AI_TEXT_EMBDEDING_MODEL_3_LARGE)
-    return embeddings
+def get_embedding_function(model_source: str = "huggingface"):
+    if model_source == "huggingface":
+        return HuggingFaceEmbeddings(model_name=HUGGING_FACE_MODEL_NAME)
+    elif model_source == "openai":
+        return OpenAIEmbeddings(model=OPEN_AI_TEXT_EMBDEDING_MODEL_3_LARGE)
+    else:
+        raise ValueError(f"Unknown model source: {model_source}")
